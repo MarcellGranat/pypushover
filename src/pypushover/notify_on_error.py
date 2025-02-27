505f67a2f4def8c7088a7f3a71d4782827367a1b
MarcellGranat <granatcellmar98@gmail.com>
2025-02-27 17:16:44 +0100

    Replace pushover_notification with send_message in error_notification function

--- src/pypushover/notify_on_error.py
+++ src/pypushover/notify_on_error.py
@@ -1,8 +1,8 @@
-from pypushover import pushover_notification
+from pypushover import send_message
 import sys
 
 def error_notification(exc_type, exc_value, exc_traceback):
-  pushover_notification("Error: " + str(exc_value))
+  send_message("Error: " + str(exc_value))
   print(exc_traceback)
 
 sys.excepthook = error_notification
