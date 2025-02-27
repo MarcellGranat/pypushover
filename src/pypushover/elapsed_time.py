8004f884ab7cdfb9b6366847cf78a21f4a4441e3
MarcellGranat <granatcellmar98@gmail.com>
2025-02-27 17:37:57 +0100

    Fix time import and update notify_after_elapsed_time function to use time() instead of now()

--- src/pypushover/elapsed_time.py
+++ src/pypushover/elapsed_time.py
@@ -1,13 +1,13 @@
 from pypushover.pushover import send_message
-from time import now
+from time import time
 
-last_time = now()
+last_time = time()
 sent_first_message = False
 
 def notify_after_elapsed_time(message: str, title: str | None = None, elapsed_time: int = 60, init = True) -> None:
     global last_time
     global sent_first_message
-    current_time = now()
+    current_time = time()
     elapsed_time = current_time - last_time
 
     if not sent_first_message and init:
@@ -19,3 +19,6 @@ def notify_after_elapsed_time(message: str, title: str | None = None, elapsed_ti
     if elapsed_time > elapsed_time:
         send_message(message=message, title=title)
         last_time = current_time
+
+if __name__ == "__main__":
+    notify_after_elapsed_time("Hello from pypushover!")
