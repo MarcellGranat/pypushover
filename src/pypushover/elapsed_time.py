f23c5b1486d908c3ed63f997e5b7a232b300397a
MarcellGranat <granatcellmar98@gmail.com>
2025-02-27 17:13:29 +0100

    Add initial message sending logic to notify_after_elapsed_time function

--- src/pypushover/elapsed_time.py
+++ src/pypushover/elapsed_time.py
@@ -2,11 +2,20 @@ from pushover import send_message
 from time import now
 
 last_time = now()
+sent_first_message = False
 
-def notify_after_elapsed_time(message: str, title: str | None = None, elapsed_time: int = 60) -> None:
+def notify_after_elapsed_time(message: str, title: str | None = None, elapsed_time: int = 60, init = True) -> None:
     global last_time
+    global sent_first_message
     current_time = now()
     elapsed_time = current_time - last_time
-    if elapsed_time > 60:
+
+    if not sent_first_message and init:
+        send_message(message=message, title=title)
+        sent_first_message = True
+        last_time = current_time
+        return
+
+    if elapsed_time > elapsed_time:
         send_message(message=message, title=title)
         last_time = current_time
