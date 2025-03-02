b2265664760f68d4a00e68fbeabf5811a6dedb67
MarcellGranat <granatcellmar98@gmail.com>
2025-03-02 16:06:35 +0100

    do not import error_notification by default

--- src/pypushover/__init__.py
+++ src/pypushover/__init__.py
@@ -1,5 +1,4 @@
 from .elapsed_time import notify_after_elapsed_time
-from .notify_on_error import error_notification
 from .pushover import send_message
 
-__all__ = ["notify_after_elapsed_time", "error_notification", "send_message"]
+__all__ = ["notify_after_elapsed_time", "send_message"]
