286efcc5bffcbe0d82b79919624ba6ca70ceeb26
MarcellGranat <granatcellmar98@gmail.com>
2025-03-03 09:20:16 +0100

    Refactor Pushover API key handling to use keys from a separate module

--- src/pypushover/pushover.py
+++ src/pypushover/pushover.py
@@ -1,16 +1,17 @@
 import os
 import requests
+from .keys import pushover_keys
 
-try:
-    api_key = os.environ['PUSHOVER_API_KEY']
-    user_key = os.environ['PUSHOVER_USER_KEY']
-except KeyError:
-    raise Exception('PUSHOVER_API_KEY and PUSHOVER_USER_KEY must be set in the environment')
+api_key, user_key = pushover_keys()
+
 
 def send_message(message: str, title: str | None = None):
     if not title:
         title = "Python"
-    
+
+    global api_key
+    global user_key
+
     try:
         # URL for the Pushover API
         url = "https://api.pushover.net/1/messages.json"
@@ -18,18 +19,17 @@ def send_message(message: str, title: str | None = None):
         data = {
             "token": api_key,
             "user": user_key,
-            "device": "iphone", # might be different 4u
+            "device": "iphone",  # might be different 4u
             "title": title,
-            "message": message
+            "message": message,
         }
 
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
         requests.post(url, data=data, headers=headers)
     except Exception as e:
         print(f"An error occurred with notification: {e}")
+
 
 if __name__ == "__main__":
     send_message("Hello from pypushover!")
