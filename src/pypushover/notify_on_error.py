e3fe710fc4e63fe61b9b8e5b38e1d0ae02c0e556
MarcellGranat <granatcellmar98@gmail.com>
2025-03-03 09:23:42 +0100

    Improve error notification handling by printing the error message instead of the traceback

--- src/pypushover/notify_on_error.py
+++ src/pypushover/notify_on_error.py
@@ -1,8 +1,10 @@
 from .pushover import send_message
 import sys
 
+
 def error_notification(exc_type, exc_value, exc_traceback):
-  send_message("Error: " + str(exc_value))
-  print(exc_traceback)
+    send_message("Error: " + str(exc_value))
+    print(str(exc_value))
+
 
 sys.excepthook = error_notification
