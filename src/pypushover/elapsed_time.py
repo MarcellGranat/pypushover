c907a939f4bf92b93e1ca0ed34d2f5013fd2555b
MarcellGranat <granatcellmar98@gmail.com>
2025-03-03 09:23:58 +0100

    Format notify_after_elapsed_time function parameters for improved readability

--- src/pypushover/elapsed_time.py
+++ src/pypushover/elapsed_time.py
@@ -4,7 +4,10 @@ from time import time
 last_time = time()
 sent_first_message = False
 
-def notify_after_elapsed_time(message: str, title: str | None = None, elapsed_time: int = 60, init = True) -> None:
+
+def notify_after_elapsed_time(
+    message: str, title: str | None = None, elapsed_time: int = 60, init=True
+) -> None:
     global last_time
     global sent_first_message
     current_time = time()
@@ -19,6 +22,7 @@ def notify_after_elapsed_time(message: str, title: str | None = None, elapsed_ti
     if elapsed_time > elapsed_time:
         send_message(message=message, title=title)
         last_time = current_time
+
 
 if __name__ == "__main__":
     notify_after_elapsed_time("Hello from pypushover!")
